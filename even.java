837d5412dc31121e766283d82b329e0ffc8ae282
Diviksha makthalin <jdivikshamakthalinb@gmail.com>
2024-07-30 22:16:18 +0530

    sum of digit,prime num even.java

--- even.java
+++ even.java
@@ -16,7 +16,7 @@ public class even {
 //        **Problem:**
 //Write a Java program that calculates and prints the sum of all multiples of 5 between 1 and 100.
 //        **Hint:** Use a `for` loop to iterate from 1 to 100 and an `if` statement to check if a number is a multiple of 5.
-public class sum {
+public class even {
      public static void main(String[] args) {
          int sum = 0;
          for(int i=1;i<=100;i++){
@@ -27,11 +27,58 @@ public class sum {
          System.out.println(sum);
          }
      }
+//### 3. Prime Number Checker
+//**Problem:**
+//Write a Java program that checks if a given number is prime. A prime number is a number that is only divisible by 1 and itself.
+//**Hint:** Use a `for` loop to iterate from 2 to the square root of the number and an `if` statement to check if it has any divisors other than 1 and itself.
+import java.util.Scanner;
+public class even{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("check: ");
+        int number = scanner.nextInt();
+        if (number <= 1 || number == 2){
+            System.out.println("it is prime num:"+number);
+        }
+        else if (number % 2 == 0){
+           System.out.println("it is  not prime num:"+number);
+        }
+        else{
+            System.out.println("it is  prime num:"+number); 
+            }
+            int sqrt = (int) Math.sqrt(number);
+        for (int i = 3; i <= sqrt; i += 2) {
+            if (number % i == 0) {
+                System.out.println("it is not prime num:"+number); 
+            }
+        }
+    }
+}
+//### 7. Sum of Digits
+//**Problem:**
+//Write a Java program that takes an integer from the user and calculates the sum of its digits.
+//**Hint:** Use a `while` loop to extract digits and an `if` statement to check the termination condition
+import java.util.Scanner;
+public class even{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter:");
+        int number = scanner.nextInt(); 
+        int sum = 0;
+        int n = number;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        System.out.println("sum of digit" + sum);
+        scanner.close();
+    }
+}
 //### extra1. Print leap year
 //**Problem:**
 //Write a Java program that prints all leap year between 2001 and 2014.
 //        **Hint:** Use a `for each` loop to iterate  and an `if` statement to check if a year is leap year.
-public class leapyear {
+public class even {
     public static void main(String[] args) {
         int[] years = {2001, 2002, 2003, 2004, 2008, 2009, 2010, 2011, 2012,2013,2014};
         for(int year : years) {
