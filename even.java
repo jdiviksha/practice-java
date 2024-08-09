31538145b5ae818a02342b4bb9d7ba9ea26d5678
Diviksha makthalin <jdivikshamakthalinb@gmail.com>
2024-08-09 22:41:20 +0530

    pattern even.java

--- even.java
+++ even.java
@@ -159,6 +159,21 @@ class HelloWorld{
         }
     }
 }
+### extra 3. pattern model 1
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
 
 
 
