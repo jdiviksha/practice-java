8ad918c4528e1d40a77c15674ea09012c147eb34
Diviksha makthalin <jdivikshamakthalinb@gmail.com>
2024-08-10 23:11:22 +0530

    2D array 1even.java

--- even.java
+++ even.java
@@ -174,7 +174,35 @@ public class Main{
         scanner.close();
     }
 }
+### extra 3. 2D arrya practice 1
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int M=scanner.nextInt();
+        int N=scanner.nextInt();
+        int[][] matrix=new int[M][N];
+        for(int i=0;i<M;i++) {
+            for(int j=0;j<N;j++) {
+                matrix[i][j]=scanner.nextInt();
+            }
+        }
+        for(int i=0;i<M;i++) {
+            if(i%2==0) {
+                for(int j=0;j<N;j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+            } else{
+                for(int j=N-1;j>=0;j--) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
 
+        scanner.close();
+    }
+}
 
 
 
